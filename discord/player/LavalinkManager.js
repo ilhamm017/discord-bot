0c81b07c4dcee21ed4a2b499750648c58f8cc75a
ilhamm017 <ilhammuliawan26@gmail.com>
2026-03-08 10:02:46 +0700

    chore: Tune Lavalink audio buffer and quality settings for improved stability and performance, and increase client position update interval.

--- discord/player/LavalinkManager.js
+++ discord/player/LavalinkManager.js
@@ -500,7 +500,7 @@ class LavalinkService {
             },
             autoSkip: true,
             playerOptions: {
-                clientBasedPositionUpdateInterval: 500,
+                clientBasedPositionUpdateInterval: 1000,
                 defaultSearchPlatform: "ytsearch",
                 volumeDecrementer: 1,
                 onDisconnect: {
